b1faea501a9049c4fa4b4f2ddf886e3858224799
studiosweb <studiosweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 06:50:11 +0000

    Version 1.6
    git-svn-id: https://plugins.svn.wordpress.org/wp-great-newsletter/tags/1.6@1197717 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/information_admin_page.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+    <div id="wp-great-newsletter">
+        <?php print __( '<h2>WP Great Newsletter</h2>', 'wp-great-newsletter' ); ?>
+        <div id="gs-middle">
+            <?php require_once( WP_GREAT_NEWSLETTER_DIR . "admin/includes/gs-middle-information-page.php" ); ?>
+        </div>    
+    </div>
+</div>
+
+
